ee8fe072bff0095212c564eecfb6980354f7e5e0
Aaron Crow <aaron@factual.com>
2012-04-24 09:17:38 -0700

    remove unused explicit guava dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :description "A Clojure driver for Factual's public API"
   :dependencies [
     [com.google.api-client/google-api-client "1.8.0-beta"]
-    [com.google.guava/guava "r09"]
     [org.clojure/clojure "1.4.0-alpha2"]
     [org.clojure/data.json "0.1.1"]
     [slingshot "0.9.0"]]
