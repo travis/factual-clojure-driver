a8b0687d102f9ea59e1c4c8b3f84ce5df257837a
Aaron Crow <aaron@factual.com>
2012-04-24 09:53:06 -0700

    upgrade slingshot to 0.10.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@
   * Removed support for CrossRef
   * Removed demo.clj and added real unit tests
   * Upgraded google-api-client to 1.8.0-beta
+  * Upgraded clojure to 1.4.0
+  * Upgraded slingshot to 0.10.2
   * Added support for Facets
   * Added support for Contribute
   * Added support for Flag
--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :description "A Clojure driver for Factual's public API"
   :dependencies [
     [com.google.api-client/google-api-client "1.8.0-beta"]
-    [org.clojure/clojure "1.4.0-alpha2"]
+    [org.clojure/clojure "1.4.0"]
     [org.clojure/data.json "0.1.1"]
-    [slingshot "0.9.0"]]
+    [slingshot "0.10.2"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
