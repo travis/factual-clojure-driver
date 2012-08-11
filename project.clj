f603ea216d7ea6b06445bccc2bfcf9627cc74223
Aaron Crow <dirtyvagabond@yahoo.com>
2012-08-10 17:42:20 -0700

    Update version to 1.4.1. Update Changelog

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject factual/factual-clojure-driver "1.4.0"
+(defproject factual/factual-clojure-driver "1.4.1"
   :url "http://github.com/Factual/factual-clojure-driver"
   :description "Officially supported Clojure driver for Factual's public API"
   :dependencies [
-    [com.google.api-client/google-api-client "1.8.0-beta"]
     [org.clojure/clojure "1.4.0"]
     [org.clojure/data.json "0.1.2"]
     [slingshot "0.10.2"]
